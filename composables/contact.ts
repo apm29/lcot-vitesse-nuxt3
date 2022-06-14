bfcc10f9a335453306b0b69e0c1f87268945a692
apm29 <835170486@qq.com>
2022-06-14 09:08:04 +0800

    fix🐛:  version

--- composables/contact.ts
+++ composables/contact.ts
@@ -4,18 +4,6 @@ enum ContactTypes {
   CONTACT_US = 'CONTACT_US',
 }
 
-export function generateUUID() {
-  const s: string[] = []
-  const hexDigits = '0123456789abcdef'
-  for (let i = 0; i < 36; i++)
-    s[i] = hexDigits.substr(Math.floor(Math.random() * 0x10), 1)
-
-  s[14] = '4' // bits 12-15 of the time_hi_and_version field to 0010
-  s[19] = hexDigits.substr((parseInt(s[19]) & 0x3) | 0x8, 1) // bits 6-7 of the clock_seq_hi_and_reserved to 01
-  s[8] = s[13] = s[18] = s[23] = '-'
-  return s.join('')
-}
-
 export function useContact() {
   const router = useRouter()
   function travelToContact(type: ContactTypes) {
@@ -26,13 +14,8 @@ export function useContact() {
       },
     })
   }
-  const uuid = ref(generateUUID())
   return {
     ContactTypes,
     travelToContact,
-    uuid,
-    regenerateUUID: () => {
-      uuid.value = generateUUID()
-    },
   }
 }
