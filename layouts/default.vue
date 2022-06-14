acac3777da56ee1b1eb678f8d7011e0cd7c29bee
yingjiawei <yjw999wow@163.com>
2022-06-14 20:57:37 +0800

    fix: main content width

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,13 +8,14 @@
     <main vertical self="center" style="min-height: calc(100vh - 10rem);" flex="~" justify="center">
       <!-- 侧边导航.次级导航 -->
       <NavSider class="md:!flex !hidden" />
-      <main>
+      <main
+        xl:w-980px
+        lg:w-860px
+        md:w-720px
+        sm:w-full
+        w-full
+      >
         <section
-          xl:w-980px
-          lg:w-860px
-          md:w-720px
-          sm:w-full
-          w-full
           bg="white"
           transition="all duration-450"
           min-h-screen
