542a1bf97602e778bbf536299c365c921b867d77
apm29 <835170486@qq.com>
2022-06-23 14:07:50 +0800

    feat: loading image

--- components/AvatarImage.vue
+++ components/AvatarImage.vue
@@ -14,32 +14,55 @@ const { isLoading } = useImage({ src: props.src })
 </script>
 
 <template>
-  <span v-if="isLoading" class="loading">Loading</span>
+  <div v-if="isLoading" class="loading !w-200px !h-160px">
+    <span class="loading-text">Loading</span>
+  </div>
   <img v-else :src="src" v-bind="$attrs">
 </template>
 
 <style scoped>
 .loading {
-    @apply text-gray-500 relative;
+  display: flex;
+  position: relative;
+  justify-content: center;
+  align-items: center;
+  color: #d3d3d3;
+  background-color: #ededed;
+  background: linear-gradient(
+      100deg,
+      rgba(255, 255, 255, 0) 40%,
+      rgba(255, 255, 255, 0.5) 50%,
+      rgba(255, 255, 255, 0) 60%
+    )
+    #ededed;
+  background-size: 200% 100%;
+  background-position-x: 180%;
+  animation: 1s loading ease-in-out infinite;
 }
 
-.loading::after {
-    content: "";
-    position: absolute;
-    top: 0%;
-    bottom: 0;
-    animation: dot 3s infinite steps(3, start);
+@keyframes loading {
+  to {
+    background-position-x: -20%;
+  }
+}
+
+.loading-text::after {
+  content: "";
+  align-items: center;
+  right: 0%;
+  bottom: 0;
+  animation: dot 3s infinite steps(3, start);
 }
 
 @keyframes dot {
-    33.33% {
-        content: ".";
-    }
-    66.67% {
-        content: "..";
-    }
-    100% {
-        content: "...";
-    }
+  33.33% {
+    content: ".";
+  }
+  66.67% {
+    content: "..";
+  }
+  100% {
+    content: "...";
+  }
 }
 </style>
